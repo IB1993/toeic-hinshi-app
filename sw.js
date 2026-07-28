4978c2f64ded5017d77b5226579cf5a70d4a4c7a
IB1993 <IB1993@users.noreply.github.com>
2026-07-29 06:38:32 +0900

    Full rebuild: remove SW caching, clean single-file HTML app

--- sw.js
+++ sw.js
@@ -1,33 +1,5 @@
-const CACHE = 'toeic-v5';
-const URLS  = [
-  '/toeic-hinshi-app/',
-  '/toeic-hinshi-app/index.html',
-];
-
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE).then(c => c.addAll(URLS)).then(() => self.skipWaiting())
-  );
-});
-
+﻿// Unregister: clear all caches
+self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', e => {
-  // Delete ALL old caches
-  e.waitUntil(
-    caches.keys()
-      .then(keys => Promise.all(keys.map(k => caches.delete(k))))
-      .then(() => self.clients.claim())
-  );
-});
-
-// Network-first: always try network, fall back to cache
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    fetch(e.request).then(res => {
-      if (res.ok) {
-        const clone = res.clone();
-        caches.open(CACHE).then(c => c.put(e.request, clone));
-      }
-      return res;
-    }).catch(() => caches.match(e.request))
-  );
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k)))).then(() => self.clients.claim()));
 });
